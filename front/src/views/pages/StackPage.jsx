6671816089f5e19808f5c8b712585561b2105a84
Romain Dreidemy <dreidemyromain@gmail.com>
2023-06-20 15:29:50 +0200

    feat(navigation): move board into StackPage

--- front/src/views/pages/StackPage.jsx
+++ front/src/views/pages/StackPage.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom";
+import BoardOrganism from "../organisms/Board.organism";
+import React from "react";
 
 const StackPage = () => {
     const { id } = useParams();
     return (
         <div>
             <h1>StackPage {id}</h1>
+            <BoardOrganism />
         </div>
     )
 }
