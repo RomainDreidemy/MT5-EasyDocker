bab6421b78a9f80176b12e69c9a115a0e3f4e5e1
Romain Dreidemy <dreidemyromain@gmail.com>
2023-06-21 13:49:50 +0200

    feat(core): configure database

--- api/src/server.go
+++ api/src/server.go
@@ -1,16 +1,28 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/RomainDreidemy/MT5-docker-extension/src/initializers"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"log"
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatalln("Failed to load environment variables! \n", err.Error())
+	}
+	initializers.ConnectDB(&config)
+}
 
 func main() {
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
-	err := app.Listen(":3000")
-	if err != nil {
-		return
-	}
+	log.Fatal(app.Listen(":3000"))
 }
