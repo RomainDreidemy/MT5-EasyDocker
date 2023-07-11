40792f0d4518dec1023d5149a39b078b28c5f397
Thomasevano <thomas-evano@hotmail.fr>
2023-07-11 15:50:07 +0200

    fix(api): db port while initializing

--- api/src/initializers/db_initializer.go
+++ api/src/initializers/db_initializer.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 
 func ConnectDB(config *Config) {
 	var err error
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Paris", config.DBHost, config.DBUserName, config.DBUserPassword, config.DBName, config.DBPort)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Paris", config.DBHost, config.DBUserName, config.DBUserPassword, config.DBName, "5432")
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
