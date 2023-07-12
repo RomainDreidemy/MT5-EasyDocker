df98f7d8db355eca94107ea43ded33b9bcb58902
10YAR <mail@diyar-bayrakli.fr>
2023-07-12 21:02:56 +0200

    lil fix

--- front/src/services/entities/Auth.entity.ts
+++ front/src/services/entities/Auth.entity.ts
@@ -1,9 +1,9 @@
 import axios from '../utils/axios'
-import { type IAuth, type IAuthEntity, type IAuthRegisterEntity } from '../../interfaces/Auth.interface'
+import { type IAuth, type IAuthEntity } from '../../interfaces/Auth.interface'
 
 const AuthEntity = (): IAuth => ({
   auth: async (data: IAuthEntity) => await axios.post('/auth/login', data),
-  register: async (data: IAuthRegisterEntity) => await axios.post('/auth/register', data)
+  register: async (data: IAuthEntity) => await axios.post('/auth/register', data)
 })
 
 export default AuthEntity()
