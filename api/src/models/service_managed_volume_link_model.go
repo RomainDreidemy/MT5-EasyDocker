c4dfe3160e625b0ca2a202a31e33ce7a50c6f98a
Romain Dreidemy <dreidemyromain@gmail.com>
2023-07-12 11:20:07 +0200

    feat(managed volume): create table to link managed volumes and services

---
+++ api/src/models/service_managed_volume_link_model.go
@@ -0,0 +1,32 @@
+package models
+
+type ServiceManagedVolumeLink struct {
+	ID        string `gorm:"primary_key;type:uuid;not null"`
+	ServiceID string `gorm:"type:uuid;not null"`
+	Service   Service
+
+	ManagedVolumeID string `gorm:"type:uuid;not null"`
+	ManagedVolume   ManagedVolume
+
+	ServiceArrowPosition       string `gorm:"type:varchar(255);not null"`
+	ManagedVolumeArrowPosition string `gorm:"type:varchar(255);not null"`
+
+	ContainerPath string `gorm:"type:varchar(255)"`
+}
+
+type ServiceManagedVolumeLinkResponse struct {
+	ID                         string `json:"id"`
+	ServiceID                  string `json:"serviceId"`
+	ManagedVolumeID            string `json:"managedVolumeId"`
+	ServiceArrowPosition       string `json:"serviceArrowPosition"`
+	ManagedVolumeArrowPosition string `json:"managedVolumeArrowPosition"`
+	ContainerPath              string `json:"containerPath"`
+}
+
+type ServiceManagedVolumeLinkCreateInput struct {
+	ServiceID                  string `json:"serviceId" validate:"required,uuid4"`
+	ManagedVolumeID            string `json:"managedVolumeId" validate:"required,uuid4"`
+	ServiceArrowPosition       string `json:"serviceArrowPosition" validate:"required,oneof=top bottom left right"`
+	ManagedVolumeArrowPosition string `json:"managedVolumeArrowPosition" validate:"required,oneof=top bottom left right"`
+	ContainerPath              string `json:"containerPath"`
+}
