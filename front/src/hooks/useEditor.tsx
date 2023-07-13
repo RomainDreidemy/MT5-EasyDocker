165f79a1fdc704c85b34f894132a0f62e0a1ecff
JyToSTT <jean.tostivint@gmail.com>
2023-07-13 14:14:49 +0200

    feat(editor): refactor

--- front/src/hooks/useEditor.tsx
+++ front/src/hooks/useEditor.tsx
@@ -1,12 +1,96 @@
 import { type TDrawer } from '../types/Drawer'
 import { useState } from 'react'
 import { type EditorForm, TYPE_STRUCTURES } from '../forms/editor.structure'
+import EventsCanvas from '../services/canvas/Events.canvas'
+import { type AxiosResponse } from 'axios'
+import { type TEntity } from '../types/Entity'
+import { DrawerTypes } from '../enums/DrawerTypes'
+import ServiceEntity from '../services/entities/Service.entity'
+import { type IService, type IServiceCreate } from '../interfaces/Service.interface'
+import NetworkEntity from '../services/entities/Network.entity'
+import { type INetwork, type INetworkCreate } from '../interfaces/Network.interface'
+import VolumeEntity from '../services/entities/Volume.entity'
+import { type IVolume, type IVolumeCreate } from '../interfaces/Volume.interface'
+import { Errors } from '../enums/errors'
+import { object } from 'yup'
+import { type TOnChange } from '../interfaces/Forms/Input.interface'
 
-const useEditor = (drawer: TDrawer): { fields: EditorForm[] } => {
+const useEditor = (drawer: TDrawer, stackId: string): {
+  fields: EditorForm[]
+  onSubmit: () => void
+  onChange: (event: TOnChange) => void
+  entityForm: TEntity
+} => {
+  const [entityForm, setEntityForm] = useState<TEntity>(drawer.entity!)
   const [structure] = useState<EditorForm[]>(TYPE_STRUCTURES[drawer.type!])
 
+  const isCreating: boolean = drawer.isCreatingEntity()
+
+  const validatorsSchema = object(structure.reduce((acc, field) =>
+    ({ [field.name]: field.validator }), {}))
+
+  const onChange: (event: TOnChange)
+  => void =
+    (event: TOnChange): void => {
+      setEntityForm({ ...entityForm, [event.target.name]: event.target.value })
+    }
+
+  const onSubmit = async (): Promise<void> => {
+    try {
+      await validatorsSchema.validate(entityForm)
+
+      entityForm.positionX = drawer.factory!.positionX
+      entityForm.positionY = drawer.factory!.positionY
+
+      const response = isCreating
+        ? await create()
+        : await update()
+
+      const { data: entity } = response
+      drawer.entity = entity
+      EventsCanvas.updateScreen()
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const create = async (): Promise<AxiosResponse<TEntity>> => {
+    switch (drawer.type) {
+      case DrawerTypes.SERVICE:
+        return await ServiceEntity.create(stackId, entityForm as IServiceCreate)
+
+      case DrawerTypes.NETWORK:
+        return await NetworkEntity.create(stackId, entityForm as INetworkCreate)
+
+      case DrawerTypes.VOLUME:
+        return await VolumeEntity.create(stackId, entityForm as IVolumeCreate)
+
+      default:
+        throw new Error(Errors.NOT_IMPLEMENTED)
+    }
+  }
+
+  const update = async (): Promise<AxiosResponse<TEntity>> => {
+    switch (drawer.type) {
+      case DrawerTypes.SERVICE:
+        return await ServiceEntity.update(entityForm as IService)
+
+      case DrawerTypes.NETWORK:
+        return await NetworkEntity.update(entityForm as INetwork)
+
+      case DrawerTypes.VOLUME:
+        return await VolumeEntity.update(entityForm as IVolume)
+
+      default:
+        throw new Error(Errors.NOT_IMPLEMENTED)
+    }
+  }
+
   return {
-    fields: structure
+    fields: structure,
+    entityForm,
+    onSubmit,
+    onChange
   }
 }
 
--- front/src/views/organisms/Editor.organism.tsx
+++ front/src/views/organisms/Editor.organism.tsx
@@ -1,92 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import useEditor from '../../hooks/useEditor'
 import { type TDrawer } from '../../types/Drawer'
 import EventsCanvas from '../../services/canvas/Events.canvas'
 import Button from '../atoms/Forms/Button.atom'
-import { object } from 'yup'
 import { type TEntity } from '../../types/Entity'
-import { type TOnChange } from '../../interfaces/Forms/Input.interface'
-import ServiceEntity from '../../services/entities/Service.entity'
-import { type IService, type IServiceCreate } from '../../interfaces/Service.interface'
-import { DrawerTypes } from '../../enums/DrawerTypes'
-import NetworkEntity from '../../services/entities/Network.entity'
-import { type INetwork, type INetworkCreate } from '../../interfaces/Network.interface'
-import VolumeEntity from '../../services/entities/Volume.entity'
-import { type IVolume, type IVolumeCreate } from '../../interfaces/Volume.interface'
-import { Errors } from '../../enums/errors'
-import { type AxiosResponse } from 'axios'
 
 const EditorOrganism = ({ drawer, stackId }: { drawer: TDrawer, stackId: string }): JSX.Element => {
-  const { fields } = useEditor(drawer)
+  const { fields, onSubmit, onChange, entityForm } = useEditor(drawer, stackId)
 
-  const [entityForm, setEntityForm] = useState<TEntity>(drawer.entity!)
-
-  const isCreating: boolean = drawer.isCreatingEntity()
-  const submitText: string = isCreating ? 'Create' : 'Update'
-
-  const validatorsSchema = object(fields.reduce((acc, field) =>
-    ({ [field.name]: field.validator }), {}))
-
-  const changeValue: (event: TOnChange)
-  => void =
-    (event: TOnChange): void => {
-      setEntityForm({ ...entityForm, [event.target.name]: event.target.value })
-    }
+  const submitText: string = drawer.isCreatingEntity() ? 'Create' : 'Update'
 
   const onClose = (): void => {
     EventsCanvas.clearSelectedDrawer()
     EventsCanvas.updateScreen()
-  }
-
-  const onSubmit = async (): Promise<void> => {
-    try {
-      await validatorsSchema.validate(entityForm)
-
-      entityForm.positionX = drawer.factory!.positionX
-      entityForm.positionY = drawer.factory!.positionY
-
-      const response = isCreating
-        ? await create()
-        : await update()
-
-      const { data: entity } = response
-      drawer.entity = entity
-      EventsCanvas.updateScreen()
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  const create = async (): Promise<AxiosResponse<TEntity>> => {
-    switch (drawer.type) {
-      case DrawerTypes.SERVICE:
-        return await ServiceEntity.create(stackId, entityForm as IServiceCreate)
-
-      case DrawerTypes.NETWORK:
-        return await NetworkEntity.create(stackId, entityForm as INetworkCreate)
-
-      case DrawerTypes.VOLUME:
-        return await VolumeEntity.create(stackId, entityForm as IVolumeCreate)
-
-      default:
-        throw new Error(Errors.NOT_IMPLEMENTED)
-    }
-  }
-
-  const update = async (): Promise<AxiosResponse<TEntity>> => {
-    switch (drawer.type) {
-      case DrawerTypes.SERVICE:
-        return await ServiceEntity.update(entityForm as IService)
-
-      case DrawerTypes.NETWORK:
-        return await NetworkEntity.update(entityForm as INetwork)
-
-      case DrawerTypes.VOLUME:
-        return await VolumeEntity.update(entityForm as IVolume)
-
-      default:
-        throw new Error(Errors.NOT_IMPLEMENTED)
-    }
   }
 
   return (
@@ -114,7 +40,7 @@ const EditorOrganism = ({ drawer, stackId }: { drawer: TDrawer, stackId: string 
               type={field.type}
               name={field.name}
               value={value as string}
-              onChange={changeValue}
+              onChange={onChange}
             />)
         })}
 
