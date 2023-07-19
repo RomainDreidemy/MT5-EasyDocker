32be16797051b4ad47f80953b8995f9d742370f7
JyToSTT <jean.tostivint@gmail.com>
2023-07-19 17:54:33 +0200

    feat(entity): adding drawer hook

--- front/src/views/organisms/Manager.organism.tsx
+++ front/src/views/organisms/Manager.organism.tsx
@@ -1,103 +1,10 @@
-import React, { useState } from 'react'
-import EventsCanvas from '../../services/canvas/Events.canvas'
-import { type IServiceCreate } from '../../interfaces/Service.interface'
+import React from 'react'
 import EntityButtonAtom from '../atoms/forms/EntityButton.atom'
-import { type INetworkCreate } from '../../interfaces/Network.interface'
-import { type IVolumeCreate } from '../../interfaces/Volume.interface'
-import { type AxiosResponse } from 'axios'
-import { type TEntity, type TEntityCreate } from '../../types/Entity'
 import { DrawerTypes } from '../../enums/DrawerTypes'
-import ServiceEntity from '../../services/entities/Service.entity'
-import NetworkEntity from '../../services/entities/Network.entity'
-import VolumeEntity from '../../services/entities/Volume.entity'
-import { Errors } from '../../enums/errors'
-import { type TServiceDrawer } from '../../types/board/drawer/Service.drawer'
-import ServiceDrawer from '../../services/board/drawer/Service.drawer'
-import NetworkDrawer from '../../services/board/drawer/Network.drawer'
-import VolumeDrawer from '../../services/board/drawer/Volume.drawer'
-import StateFactory from '../../services/board/drawer/factories/State.factory'
-import { type ISize } from '../../interfaces/Window.interface'
+import useDrawerManager from '../../hooks/useDrawerManager'
 
 const ManagerOrganism = ({ stackId }: { stackId: string }): JSX.Element => {
-  const [loading, setLoading] = useState<boolean>(false)
-
-  const entityForm = {
-    [DrawerTypes.SERVICE]: {
-      drawer: ServiceDrawer,
-      form: {
-        description: '',
-        dockerImage: '',
-        dockerTag: '',
-        entrypoint: '',
-        isExternal: false,
-        positionX: 0,
-        positionY: 0,
-        name: 'Unnamed'
-      }
-    },
-    [DrawerTypes.NETWORK]: {
-      drawer: NetworkDrawer,
-      form: {
-        isExternal: false,
-        name: 'Unnamed',
-        positionX: 0,
-        positionY: 0
-      }
-    },
-    [DrawerTypes.VOLUME]: {
-      drawer: VolumeDrawer,
-      form: {
-        containerPath: '',
-        description: '',
-        localPath: '',
-        name: 'Unnamed',
-        positionX: 0,
-        positionY: 0
-      }
-    }
-  }
-
-  const createEntity = async (entity: TEntityCreate, type: DrawerTypes): Promise<AxiosResponse<TEntity>> => {
-    switch (type) {
-      case DrawerTypes.SERVICE:
-        return await ServiceEntity.create(stackId, entity as IServiceCreate)
-
-      case DrawerTypes.NETWORK:
-        return await NetworkEntity.create(stackId, entity as INetworkCreate)
-
-      case DrawerTypes.VOLUME:
-        return await VolumeEntity.create(stackId, entity as IVolumeCreate)
-
-      default:
-        throw new Error(Errors.NOT_IMPLEMENTED)
-    }
-  }
-
-  const createEntityAndDraw = async (type: DrawerTypes): Promise<void> => {
-    try {
-      setLoading(true)
-
-      const { form, drawer } = entityForm[type]
-
-      const size: ISize = { width: StateFactory.width, height: StateFactory.height }
-
-      const { x, y } = EventsCanvas.emptyPosition(size)
-
-      form.positionX = x
-      form.positionY = y
-
-      const { data: entityCreated } = await createEntity(form, type)
-
-      const entityDrawer: TServiceDrawer = drawer(entityCreated, EventsCanvas.context!)
-      entityDrawer.create()
-
-      EventsCanvas.addAndSelectNewDrawer(entityDrawer)
-    } catch (err) {
-      console.error(err)
-    } finally {
-      setLoading(false)
-    }
-  }
+  const { createEntityAndDraw, loading } = useDrawerManager(stackId)
 
   const createService = async (): Promise<void> => {
     await createEntityAndDraw(DrawerTypes.SERVICE)
