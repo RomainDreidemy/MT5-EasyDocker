1272dd3840521ca262ca16846bce84d24f7df3f2
JyTosTT <44032246+JyTosTT@users.noreply.github.com>
2023-07-19 14:28:44 +0200

    feat/canvas-linkers (#64)
    * feat(linker): delete a linker
    
    * feat(linker): linker rules
    
    * feat(linker): linker rules

--- front/src/services/board/drawer/Volume.drawer.ts
+++ front/src/services/board/drawer/Volume.drawer.ts
@@ -2,19 +2,19 @@ import BaseDrawer from './Base.drawer'
 import ConnectorBuilder from './connector/Connector.builder'
 import CommonConnector from './connector/Common.connector'
 import CommonLinker from './linkers/Common.linker'
-import { type IVolume, type IVolumeCreate } from '../../../interfaces/Volume.interface'
-import { type TVolumeDrawer } from '../../../types/board/drawer/Volume.drawer'
+import {type TVolumeDrawer} from '../../../types/board/drawer/Volume.drawer'
 import VolumeFactory from './factories/Volume.factory'
-import { DrawerTypes } from '../../../enums/DrawerTypes'
+import {DrawerTypes} from '../../../enums/DrawerTypes'
+import {TEntityOrCreate} from "../../../types/Entity";
 
-const VolumeDrawer = (volume: IVolume | IVolumeCreate, context: CanvasRenderingContext2D): TVolumeDrawer => {
+const VolumeDrawer = (volume: TEntityOrCreate, context: CanvasRenderingContext2D): TVolumeDrawer => {
   return {
     ...BaseDrawer(),
 
     canBeLinkedWith: [DrawerTypes.SERVICE],
     type: DrawerTypes.VOLUME,
 
-    create () {
+    create() {
       this.entity = volume
       this.context = context
       this.Linker = CommonLinker
--- front/src/services/canvas/Base.manager.ts
+++ front/src/services/canvas/Base.manager.ts
@@ -1,26 +1,34 @@
-import { type TBaseManager } from '../../types/canvas/Base.manager'
-import { type TDrawer, type TDrawers } from '../../types/Drawer'
+import {type TBaseManager} from '../../types/canvas/Base.manager'
+import {type TDrawer, type TDrawers} from '../../types/Drawer'
 import BaseCanvas from './Base.canvas'
 import StateCanvas from './State.canvas'
-import { DrawerManager } from './Drawer.manager'
+import {DrawerManager} from './Drawer.manager'
 import eventEmitter from '../apps/Event.emitter'
-import { EventEmitters } from '../../enums/eventEmitters'
+import {EventEmitters} from '../../enums/eventEmitters'
 
 const BaseManager: TBaseManager = {
   ...BaseCanvas,
   ...StateCanvas,
   ...DrawerManager,
 
-  add (...drawers: TDrawers): void {
+  add(...drawers: TDrawers): void {
     this.drawers.push(...drawers)
   },
 
-  deleteDrawer (drawer: TDrawer): void {
+  deleteDrawer(drawer: TDrawer): void {
     this.drawers = this.drawers.filter(d => d !== drawer)
+
+    this.deleteLinkers(drawer)
     this.updateScreen()
   },
 
-  addAndSelectNewDrawer (drawer: TDrawer): void {
+  deleteLinkers(drawer: TDrawer): void {
+    this.drawers.forEach(d => {
+      d.linkers = d.linkers.filter(linker => linker.link!.to.drawer !== drawer)
+    })
+  },
+
+  addAndSelectNewDrawer(drawer: TDrawer): void {
     this.drawers.push(drawer)
     this.selectDrawer(drawer)
     this.updateScreen()
@@ -28,13 +36,13 @@ const BaseManager: TBaseManager = {
     eventEmitter.emit(EventEmitters.ON_SELECTED_DRAWER, drawer)
   },
 
-  draw (): void {
+  draw(): void {
     this.drawers.forEach(drawer => {
       drawer.draw()
     })
   },
 
-  updateScreen (): void {
+  updateScreen(): void {
     this.clearArea()
     this.draw()
   }
