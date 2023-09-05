0554e0c06980d14cf5751ab51facdff1cfde6134
Romain Dreidemy <dreidemyromain@gmail.com>
2023-09-05 18:20:46 +0200

    feat(stack): I can create a stack

--- front/src/services/entities/Stack.entity.ts
+++ front/src/services/entities/Stack.entity.ts
@@ -1,10 +1,12 @@
 import axios from '../utils/axios'
 import { type IBoard } from '../../interfaces/Board.interface'
 import { type AxiosResponse } from 'axios'
+import { type IStack } from '../../interfaces/Stack.interface'
 
 const StackEntity = {
   stacks: async () => await axios.get('/stacks'),
   stack: async (id: string) => await axios.get(`/stacks/${id}`),
+  create: async (name: string) => await axios.post<IStack>('/stacks', { name }),
   board: async (id: string): Promise<AxiosResponse<IBoard>> => await axios.get(`/stacks/${id}/board`)
 }
 
