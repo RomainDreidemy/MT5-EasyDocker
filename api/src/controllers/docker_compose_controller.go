cf0a79bf4baf974be8afa34c487b7d9c57b0f4b1
Romain Dreidemy <45939424+RomainDreidemy@users.noreply.github.com>
2023-09-05 11:01:06 +0200

    feat(yaml): use stack id to build docker compose file (#85)

--- api/src/controllers/docker_compose_controller.go
+++ api/src/controllers/docker_compose_controller.go
@@ -1,13 +1,21 @@
 package controllers
 
 import (
+	"github.com/RomainDreidemy/MT5-docker-extension/src/models"
+	"github.com/RomainDreidemy/MT5-docker-extension/src/policies"
 	"github.com/RomainDreidemy/MT5-docker-extension/src/repositories"
 	"github.com/RomainDreidemy/MT5-docker-extension/src/services/docker_compose"
 	"github.com/gofiber/fiber/v2"
 )
 
 func GenerateDockerComposeFile(c *fiber.Ctx) error {
-	stackId := "26cf2081-b4b5-41b0-9246-af3b8e93894b"
+	stackId := c.Params("stackId")
+	currentUser := c.Locals("user").(models.UserResponse)
+
+	if !policies.CanAccessStack(currentUser, stackId) {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"status": "error", "message": "Stack not found"})
+	}
+
 	services, _ := repositories.FindServicesByStackIdWithAssociation(stackId)
 	networks, _ := repositories.FindNetworksByStackId(stackId)
 
