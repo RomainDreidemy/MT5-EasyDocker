1e05416b76ef4bd9de7ddba449f35e9efc00ed3c
Romain Dreidemy <dreidemyromain@gmail.com>
2023-09-08 12:06:00 +0200

    feat(stack): refactor
    Co-authored-by: Thomasevano <thomas-evano@hotmail.fr>

--- front/src/views/organisms/StackFormModal.organism.tsx
+++ front/src/views/organisms/StackFormModal.organism.tsx
@@ -1,18 +1,17 @@
-import React, { type Dispatch, type SetStateAction, useState } from 'react'
+import React, { useState } from 'react'
 import { type IStack, type IStackCreate, STACK_STRUCTURE } from '../../interfaces/Stack.interface'
 import Input from '../atoms/forms/Input.atom'
-import StackEntity from '../../services/entities/Stack.entity'
 import useForm from '../../hooks/useForm'
 import { type EditorForm } from '../../forms/editor.structure'
 import TextArea from '../atoms/forms/TextArea.atom'
 import ModalOrganism from './Modal.organism'
-import Button from '../atoms/forms/Button.atom'
 
-const StackFormModalOrganism = ({ stack, stacks, setStacks, toggle }: {
+const StackFormModalOrganism = ({ title, buttonText, stack, toggle, onSubmit }: {
+  title: string
+  buttonText: string
   toggle: () => void
   stack?: IStack
-  stacks: IStack[]
-  setStacks: Dispatch<SetStateAction<IStack[]>>
+  onSubmit: (form: IStackCreate) => Promise<void>
 }): JSX.Element => {
   const [structure] = useState<EditorForm[]>(STACK_STRUCTURE)
 
@@ -23,38 +22,14 @@ const StackFormModalOrganism = ({ stack, stacks, setStacks, toggle }: {
 
   const { form: stackEntityForm, onChange, validatorsSchema } = useForm<IStackCreate>(stack ?? initialForm, structure)
 
-  const isCreating = stack?.id == null
-
-  const onSubmit = async (): Promise<void> => {
+  const onSubmitWithValidation = async (): Promise<void> => {
     await validatorsSchema.validate(stackEntityForm)
 
-    const { data: stackResponse} =
-      isCreating
-        ? await StackEntity.create(stackEntityForm)
-        : await StackEntity.update(stackEntityForm as IStack)
-
-    toggle()
-    const filteredStacks = stacks.filter((s) => s.id !== stackResponse.id)
-    
-    if (!isCreating) {
-      const indexOfEditedStack = stacks.findIndex((s) => s.id === stackResponse.id)
-      filteredStacks.splice(indexOfEditedStack, 0, stackResponse)
-      setStacks(filteredStacks)
-      return
-    }
-
-    setStacks([...filteredStacks, stackResponse])
+    await onSubmit(stackEntityForm)
   }
 
-  const buttonText =
-    isCreating
-      ? 'Create stack +'
-      : 'Edit'
-
-  const title = isCreating ? 'Create a new stack' : 'Edit stack'
-
   return (
-    <ModalOrganism toggle={toggle} onSubmit={onSubmit} buttonText={buttonText} title={title}>
+    <ModalOrganism toggle={toggle} onSubmit={onSubmitWithValidation} buttonText={buttonText} title={title}>
       <div className="relative p-6 flex-auto">
         <form>
           <Input
@@ -75,15 +50,6 @@ const StackFormModalOrganism = ({ stack, stacks, setStacks, toggle }: {
             onChange={onChange}
           />
         </form>
-        <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-          <Button
-            label={buttonText}
-            onClick={onSubmit}
-            direction={'right'}
-          />
-
-          <Button className='btn-ghost ml-4' onClick={toggle} label={'Close'} />
-        </div>
       </div>
     </ModalOrganism>
   )
