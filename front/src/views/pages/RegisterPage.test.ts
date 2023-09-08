9996576ec12e9f19617c3c3439cb6114f53a11fe
JyTosTT <44032246+JyTosTT@users.noreply.github.com>
2023-09-08 10:21:13 +0200

    fix(stacks): redirection (#106)

--- front/src/views/pages/RegisterPage.test.ts
+++ front/src/views/pages/RegisterPage.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import { validateSchema } from '../../services/utils/validation.util'
-import { IAuthEntity } from '../../interfaces/Auth.interface';
-import { object, string, ref } from 'yup';
+import { type IAuthEntity } from '../../interfaces/Auth.interface'
+import { object, string, ref } from 'yup'
 
 const registerSchema = object({
   email: string().email().required(),
@@ -10,66 +10,59 @@ const registerSchema = object({
 })
 
 test('Register - Email Validation Test', async () => {
-
   const form: IAuthEntity = {
     email: '',
     password: 'test1234',
-    passwordConfirm: 'test1234',
+    passwordConfirm: 'test1234'
   }
 
-  const expectedError =  [
+  const expectedError = [
     {
-      message: "email is a required field",
-      path: "email",
+      message: 'email is a required field',
+      path: 'email'
     }
   ]
 
   const validationErrors = (await validateSchema(registerSchema, form))
   expect(validationErrors).toMatchObject(expectedError)
-
-});
-
+})
 
 test('Register - Password Validation Test', async () => {
-
   const form: IAuthEntity = {
     email: 'test@example.com',
     password: 'test',
-    passwordConfirm: 'test',
+    passwordConfirm: 'test'
   }
 
-  const expectedError =  [
+  const expectedError = [
     {
-        message: "passwordConfirm must be at least 8 characters",
-        path: "passwordConfirm",
+      message: 'passwordConfirm must be at least 8 characters',
+      path: 'passwordConfirm'
     },
     {
-        path: 'password',
-        message: 'password must be at least 8 characters'
+      path: 'password',
+      message: 'password must be at least 8 characters'
     }
   ]
 
   const validationErrors = (await validateSchema(registerSchema, form))
   expect(validationErrors).toMatchObject(expectedError)
-});
-
+})
 
 test('Register - Password Confirm Validation Test', async () => {
+  const form: IAuthEntity = {
+    email: 'test@example.com',
+    password: 'test1234',
+    passwordConfirm: 'test4321'
+  }
 
-    const form: IAuthEntity = {
-      email: 'test@example.com',
-      password: 'test1234',
-      passwordConfirm: 'test4321',
+  const expectedError = [
+    {
+      message: 'Passwords must match',
+      path: 'passwordConfirm'
     }
+  ]
 
-    const expectedError =  [
-      {
-        message: "Passwords must match",
-        path: "passwordConfirm",
-      }
-    ]
-
-    const validationErrors = (await validateSchema(registerSchema, form))
-    expect(validationErrors).toMatchObject(expectedError)
-});
-
+  const validationErrors = (await validateSchema(registerSchema, form))
+  expect(validationErrors).toMatchObject(expectedError)
+})
