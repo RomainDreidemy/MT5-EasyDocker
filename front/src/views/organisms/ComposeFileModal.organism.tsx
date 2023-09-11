f46b78b409b120f5af85f1c07b20c3044c6cb401
JyToSTT <jean.tostivint@gmail.com>
2023-09-11 12:28:31 +0200

    feat(entity): adding downloader

--- front/src/views/organisms/ComposeFileModal.organism.tsx
+++ front/src/views/organisms/ComposeFileModal.organism.tsx
@@ -1,5 +1,7 @@
 import React, { type ReactElement } from 'react'
 import ModalOrganism from './Modal.organism'
+import Button from '../atoms/forms/Button.atom'
+import useDownload from '../../hooks/useDownload'
 
 interface ComposeFileModalOrganismProps {
   toggle: () => void
@@ -7,9 +9,17 @@ interface ComposeFileModalOrganismProps {
 }
 
 const ComposeFileModalOrganism = ({ toggle, composeFileData }: ComposeFileModalOrganismProps): ReactElement => {
+  const { onDownload } = useDownload(composeFileData)
+
+  const onDownloadClick = (): void => {
+    onDownload('docker-compose', 'yml')
+  }
+
   return (
     <ModalOrganism toggle={toggle}>
         <textarea className="h-96 w-full" readOnly value={composeFileData} />
+
+        <Button label={'Download'} className='w-auto' variant='ghost' onClick={onDownloadClick}/>
     </ModalOrganism>
   )
 }
