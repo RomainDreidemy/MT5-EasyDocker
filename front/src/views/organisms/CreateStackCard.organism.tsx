a060c9157b5f5370060ee6a013a22189b31d5a34
ThomasE <thomas-evano@hotmail.fr>
2023-09-11 11:18:41 +0200

    Refacto components organization (#111)
    * refacto(front): move Alert component in atoms
    
    * refacto(front): create component for create stack card
    
    * refacto(front): create generic Card component

---
+++ front/src/views/organisms/CreateStackCard.organism.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const StackCardOrganism = ({ onOpenModal }: {
+  onOpenModal: (undefined: undefined) => void
+}): JSX.Element => {
+  return (
+    <button
+      onClick={() => { onOpenModal(undefined) }}
+      className="card shadow-md mb-2 rounded bg-blue-100 border border-blue-100 hover:border-blue-200">
+      <div className="card-body">
+        <h2 className="card-title">+ Create a new stack</h2>
+        <p>Click here to create a new stack</p>
+      </div>
+    </button>
+  );
+}
+
+export default StackCardOrganism;
--- front/src/views/organisms/StackCard.organism.tsx
+++ front/src/views/organisms/StackCard.organism.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { type IStack } from '../../interfaces/Stack.interface'
-import Button from '../atoms/forms/Button.atom'
-import { BiTrash, BiEdit, BiCopy } from 'react-icons/bi'
+import CardOrganism from './Card.organism'
 
 const StackCardOrganism = ({ stack, id, name, description, onEdit, onDelete, onDuplicate }: {
   stack: IStack
@@ -13,22 +11,18 @@ const StackCardOrganism = ({ stack, id, name, description, onEdit, onDelete, onD
   onDelete: (id: string) => void
   onDuplicate: (id: string) => void
 }): JSX.Element => {
+
   return (
-    <div className="card shadow-md mb-2 rounded border border-blue-100 hover:border-blue-200">
-      <Link to={`/stacks/${id}`} key={id}>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p className="truncate">{description}</p>
-        </div>
-      </Link>
-      <div className="card-actions justify-end">
-        <Button className="btn-ghost" icon={<BiTrash/>} onClick={() => { onDelete(id) }} />
-
-        <Button className="btn-ghost" icon={<BiEdit/>} onClick={() => { onEdit(stack) }} />
-
-        <Button className="btn-ghost" icon={<BiCopy/>} onClick={() => { onDuplicate(id) }} />
-      </div>
-    </div>
+    <CardOrganism
+      id={id}
+      name={name}
+      description={description}
+      linkUrl={'/stacks'}
+      isEditable={true}
+      onDelete={onDelete}
+      onEdit={() => onEdit(stack)}
+      onDuplicate={onDuplicate}
+    />
   )
 }
 
