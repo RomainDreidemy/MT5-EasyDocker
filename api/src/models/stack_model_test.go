38f84d58b5e0ce0f6c8a5dd816177d74094d7389
Thomasevano <thomas-evano@hotmail.fr>
2023-10-09 17:07:06 +0200

    fix(api): stack_model_test wasn't in models_test package

--- api/src/models/stack_model_test.go
+++ api/src/models/stack_model_test.go
@@ -1,21 +1,22 @@
-package models
+package models_test
 
 import (
+	"github.com/RomainDreidemy/MT5-docker-extension/src/models"
 	"github.com/RomainDreidemy/MT5-docker-extension/src/test_helpers"
 	"testing"
 )
 
 func TestStackCreateInput(t *testing.T) {
 	t.Run("Name should be required", func(t *testing.T) {
-		stack := StackCreateInput{Name: ""}
+		stack := models.StackCreateInput{Name: ""}
 		test_helpers.AsserNotValidation(t, stack)
 	})
 	t.Run("Name should be at least 3 characters", func(t *testing.T) {
-		stack := StackCreateInput{Name: "ab"}
+		stack := models.StackCreateInput{Name: "ab"}
 		test_helpers.AsserNotValidation(t, stack)
 	})
 	t.Run("Stack should be valid when name at least 3 characters", func(t *testing.T) {
-		stack := StackCreateInput{Name: "abc"}
+		stack := models.StackCreateInput{Name: "abc"}
 		test_helpers.AssertValidation(t, stack)
 	})
 }
