32d6febf11cbe105d11e1a0fc7430c1c897dca6f
isaoud <imedsaoud@factonics.com>
2023-10-15 14:17:32 +0200

    feat/display_dockerhub_images_and_tags : add controller for dockerhub search with images logic and dockerhub images response model

---
+++ api/src/controllers/dockerhub_controller.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+		"github.com/RomainDreidemy/MT5-docker-extension/src/models"
+    "github.com/gofiber/fiber/v2"
+    "net/http"
+    "encoding/json"
+		"fmt"
+)
+
+// SearchDockerHubImages godoc
+// @Summary      Search Docker Hub for images
+// @Tags         DockerHub
+// @Accept       json
+// @Produce      json
+// @Param term query string true "Search Term"
+// @Success      200  {json}  models.DockerHubSearchResponse
+// @Router       /dockerhub/images [get]
+func SearchDockerHubImages(c *fiber.Ctx) error {
+    term := c.Query("term")
+		fmt.Println("Term =>",term)
+
+    // Effectuez la recherche d'images Docker sur Docker Hub
+    apiUrl := "https://hub.docker.com/api/content/v1/products/search?page_size=25&q=" + term
+		fmt.Println("Requested URL =>",apiUrl)
+
+    resp, err := http.Get(apiUrl)
+
+    if err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error":   "Internal Server Error",
+            "message": "Failed to search Docker Hub for images",
+        })
+    }
+    defer resp.Body.Close()
+
+    var searchResult models.DockerHubSearchResponse
+    
+
+    decoder := json.NewDecoder(resp.Body)
+
+    if err := decoder.Decode(&searchResult); err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error":   "Internal Server Error",
+            "message": "Failed to decode JSON response",
+        })
+    }
+
+
+    return c.Status(fiber.StatusOK).JSON(searchResult)
+}
